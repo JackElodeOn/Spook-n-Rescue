b63f02c8548fadb38d6403b1d349d57eba28636f
qinzhounick <qinzhounick@wustl.edu>
2024-10-07 12:34:28 -0500

    updated end scene

--- Assets/Code/UI/MenuController.cs
+++ Assets/Code/UI/MenuController.cs
@@ -29,7 +29,7 @@ public class MenuController : MonoBehaviour
 
     public void BackToMain()
     {
-        SceneManager.LoadScene("StartMenu");
+        SceneManager.LoadScene("StartMenuScene");
     }
 
 }
